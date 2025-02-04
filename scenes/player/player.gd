065f1d2d6a8eb055aab46c1f230fea0698e249b9
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2025-02-04 12:28:14 +0300

    player movement

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+const MAX_SPEED = 200
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +10,18 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var movement_vector = get_movement_vector()
+	# let's normalize vector (make vector length 1) so I able to change movement speed easily
+	var direction = movement_vector.normalized()
+	
+	# coming from CharacterBody2D
+	velocity = direction * MAX_SPEED
+	move_and_slide()
+
+func get_movement_vector():
+	# get_action_strength -> always returns 0 or 1 depends on which key pressed
+	# if I press D key (right) move_left value will be 0 and x_movement will be 1
+	# if I press A key(left) move_right will be 0 and x_movement will be -1
+	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	return Vector2(x_movement, y_movement)
